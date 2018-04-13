eb5c1230608d74a093785a85e84c44f75ade2d78
MSI\user <semmyking@gmail.com>
2018-04-13 13:39:55 +0100

    change 1.0
    lol

---
+++ healthbar/Assets/Healthbar.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class Healthbar : MonoBehaviour
+{
+    //variabelen
+    public float TotalHp;
+    public float CurrentHp;
+
+
+	void Start ()
+	{
+	    CurrentHp = TotalHp;
+	}
+	
+	void Update () {
+
+	    if (Input.GetMouseButtonDown(0))
+	    {
+	        if (CurrentHp > 0)
+	        {
+	            TakeDamage();
+	        }
+	        else
+	        {
+	            print("Dead");
+	        }
+	    }
+
+    }
+    // scale van healtbar ( x as veranderen )
+    void TakeDamage()
+    {
+        CurrentHp -= 5;
+        transform.localScale = new Vector3((CurrentHp / TotalHp), 1, 1);
+    }
+
+    
+    
+    
+}
---
+++ healthbar/Assets/death.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net.Mime;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class death : MonoBehaviour
+{
+    public int health;
+    public Text healthBar;
+
+	// Use this for initialization
+	private void Start()
+	{
+	    health = 10;
+	}
+
+    public void minHealth()
+    {
+        health--;
+        print("lol");
+        switch (health)
+        {
+            case 10:
+                 healthBar.text = "Health: 0000000000";
+                 break;
+            case 9:
+                 healthBar.text = "Health: 000000000";
+                 break;
+            case 8:
+                 healthBar.text = "Health: 00000000";
+                 break;
+            case 7:
+                 healthBar.text = "Health: 0000000";
+                 break;
+            case 6:
+                 healthBar.text = "Health: 000000";
+                 break;
+            case 5:
+                 healthBar.text = "Health: 00000";
+                 break;
+            case 4:
+                 healthBar.text = "Health: 0000";
+                 break;
+            case 3:
+                 healthBar.text = "Health: 000";
+                 break;
+            case 2:
+                 healthBar.text = "Health: 00";
+                 break;
+            case 1:
+                 healthBar.text = "Health: 0";
+                 break;
+            case 0: 
+                 healthBar.text = "Health: RIP";
+                 break;
+
+        }
+    }
+}
